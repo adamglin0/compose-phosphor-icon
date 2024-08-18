897c1d8e746d9a5e18f0a889763b8f1d1793ad2b
adamglin <dev@adamglin.com>
2024-08-18 21:50:10 +0800

    WIP

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+rootProject.name = "phosphor-icon"
+include(":phosphor")
+
+// apps
+rootDir.resolve("./sample").listFiles()?.forEach {
+    if (it.isDirectory) {
+        include(":sample:${it.name}")
+    }
+}
