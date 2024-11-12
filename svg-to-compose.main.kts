2540f8dfaa5d24cf413eb828254da77438efcdb1
adamglin <wwalkingg@gmail.com>
2024-11-12 16:54:19 +0800

    add a svg2compose kts

---
+++ svg-to-compose.main.kts
@@ -0,0 +1,29 @@
+@file:Repository("https://jitpack.io")
+@file:Repository("https://maven.google.com")
+@file:Repository("https://repo.maven.apache.org/maven2")
+
+@file:DependsOn("com.github.DevSrSouza:svg-to-compose:-SNAPSHOT")
+@file:DependsOn("com.google.guava:guava:23.1-jre")
+@file:DependsOn("com.android.tools:sdk-common:27.2.0-alpha16")
+@file:DependsOn("com.android.tools:common:27.2.0-alpha16")
+@file:DependsOn("com.squareup:kotlinpoet:1.7.2")
+@file:DependsOn("org.ogce:xpp3:1.1.6")
+
+import br.com.devsrsouza.svg2compose.Svg2Compose
+import br.com.devsrsouza.svg2compose.VectorType
+import java.io.File
+
+val assetsDir = File("./temp/phosphor-icons/SVGs Flat/")
+val srcDir = File("phosphor1/src/commonMain/kotlin/com")
+
+Svg2Compose.parse(
+    applicationIconPackage = "adamglin",
+    accessorName = "PhosphorIcons",
+    outputSourceDirectory = srcDir,
+    vectorsDirectory = assetsDir,
+    type = VectorType.SVG,
+    allAssetsPropertyName = "AllIcons",
+    iconNameTransformer = { s1, s2 ->
+        return@parse s1
+    }
+)
